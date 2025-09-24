1ebac240519d4a4253805fdb642f6f15d91a35f4
Ramaiah Padala <ramaiahpadala888@gmail.com>
2025-09-24 12:03:40 +0530

    Initial commit

---
+++ my-node-app/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! CI/CD Pipeline is working 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
